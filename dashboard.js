d4497563e57c1ba1b0d907ed3934ccbf2ffb17ab
LotalGhana <114388126+LotalGhana@users.noreply.github.com>
2025-06-18 14:35:14 -0700

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,18 +1,1 @@
-form.addEventListener('submit', async function (e) {
-  e.preventDefault();
 
-  const title = document.getElementById('title').value;
-  const platform = document.getElementById('platform').value;
-  const date = document.getElementById('date').value;
-  const earnings = 0.015; // fixed earnings per article
-
-  await db.collection('users').doc(userIdRef.uid).collection('articles').add({
-    title,
-    platform,
-    date,
-    earnings
-  });
-
-  form.reset();
-  loadArticles();
-});
